439beb4ad1d9538ef6493b211a9ac6aeb9936d41
25Simone <dantsimone111@gmail.com>
2021-12-21 14:57:37 +0100

    script base completato

--- js/script.js
+++ js/script.js
@@ -1,11 +1,36 @@
+let userNum;
+// dichiaro l'array dei numerio casuali
+const numList = [];
+// dichiaro l'array dei numeri indovinati
+const rightNum = [];
+
 // definisco le variabili di collegamento all'html
 const outputHtml = document.querySelector('.output');
 
 // funzione per generare numeri random
 getRandomNumber = (min, max) => Math.floor(Math.random() * ((max + 1) - min)) + min;
 
+setTimeout(() =>{
+    outputHtml.classList.add('d-none');
+}, 30000)
 
-// genero 5 numeri casuali e li stampo in output
+// chiedo all'utente di riscrivere i numeri visti a schermo
+setTimeout(() => {
+    for(let i = 0; i < 5; i++) {
+        userNum = parseInt(prompt('Inserisci uno alla volta i numeri appena visti'));
+        if(numList.includes(userNum)) {
+            rightNum.push(userNum);
+        }
+    }
+}, 31000)
+
+// genero 5 numeri casuali ed unici e li stampo in output
 for(let i = 0; i < 5; i++){
-    outputHtml.append(`${getRandomNumber(1, 50)} - `);
+    let n = getRandomNumber(1, 100);
+    if(!numList.includes(n)){
+        numList.push(n);
+    }
 }
+outputHtml.append(`${numList}`);
+
+
